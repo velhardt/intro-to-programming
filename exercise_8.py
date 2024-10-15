d77dc3e33dc08f7de35450d47ea98ec50813bee2
velhardt <110742984+velhardt@users.noreply.github.com>
2024-10-15 10:42:42 +0400

    Update exercise_8.py

--- exercise_8.py
+++ exercise_8.py
@@ -5,8 +5,15 @@ Author: Ethan Grajo
 Date completed: September 18 2024
 """
 
-names = ["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"]
-def search(list, query):
+names = ["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"] # the base list of names
+
+"""
+in this project, i tried 2 different methods to make a scanning program,
+one with a while loop, and one with a for loop,
+both of these functions make use of the same variables, and
+samme methods of checking the names
+"""
+def search(list, query): #similar to the second function, i make use of two parameters, the given list to search in, and the query to look for,
     i = 0
     while i < len(list):
         if list[i] == query:
